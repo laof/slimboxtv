60d4bf8e5efbae0c42dfdc86ed4ef87197865b19
denglan <18030893186@163.com>
2022-10-29 10:04:01 +0800

    add

--- assets/canvas.js
+++ assets/canvas.js
@@ -1,39 +1,15 @@
-window.inputid = ''
-
-function startCanvas() {
-  //获取随机验证码
-  var randomChars = randomChar()
-  console.log(randomChars)
-  //点击画布刷新验证码
-  document.querySelector('canvas').onclick = function () {
-    randomChars = randomChar()
-    console.log(randomChars)
-  }
-}
-//生成随机验证码
 function randomChar() {
-  //画布宽
   var canWidth = 200
-  //画布高
   var canHeight = 80
-  //存储随机字符数量
   var charCount = 4
-  //存储字体最小尺寸
   var charMinSize = 20
-  //存储字体最大尺寸
   var charMaxSize = 40
-  //存储干扰线条数
   var lineCount = 20
-  //获取画布对象
   var can = document.querySelector('canvas')
-  //获取画笔
   var ctx = can.getContext('2d')
-  //应用画布宽高
   can.width = canWidth
   can.height = canHeight
-  //产生随机验证码
   var randomChar = createRandomChars(charCount)
-  //绘制字符
   drawChars(
     randomChar,
     charMaxSize,
@@ -43,38 +19,26 @@ function randomChar() {
     charCount,
     ctx
   )
-  //绘制干扰线
   drawLine(lineCount, canWidth, canHeight, ctx)
   return randomChar.join('')
 }
-//绘制干扰线
 function drawLine(lineCount, canWidth, canHeight, ctx) {
   for (var i = 0; i < lineCount; i++) {
     ctx.beginPath()
-    //确定线条起点
-    //x:[0,canWidth] y:[0,canHeight]
     var starX = Math.ceil(Math.random() * canWidth)
     var starY = Math.ceil(Math.random() * canHeight)
     ctx.moveTo(starX, starY)
-    //确定线条终点
-    //x:[0,canWidth] y:[0,canHeight]
     var endX = Math.ceil(Math.random() * canWidth)
     var endY = Math.ceil(Math.random() * canHeight)
     ctx.lineTo(endX, endY)
-    //设置干扰线颜色
-    //随机red [0,255]
     var red = Math.ceil(Math.random() * 255)
-    //随机green [0,255]
     var green = Math.ceil(Math.random() * 255)
-    //随机blue [0,255]
     var blue = Math.ceil(Math.random() * 255)
     ctx.strokeStyle = `rgb(${red},${green},${blue})`
-    //呈现
     ctx.stroke()
     ctx.closePath()
   }
 }
-//绘制验证码到画布上
 function drawChars(
   randomChar,
   charMaxSize,
@@ -84,63 +48,34 @@ function drawChars(
   charCount,
   ctx
 ) {
-  //遍历随机字符
   for (var index in randomChar) {
     index = Number(index)
-    //获取每个字符
     var char = randomChar[index]
-    //随机设置字体尺寸 [charMinSize,charMaxSize]
     var fontSize =
       Math.ceil(Math.random() * (charMaxSize - charMinSize)) + charMinSize
-    //设置字体尺寸
     ctx.font = `${fontSize}px 微软雅黑`
-    //设置字体颜色
-    //随机red [0,255]
     var red = Math.ceil(Math.random() * 255)
-    //随机green [0,255]
     var green = Math.ceil(Math.random() * 255)
-    //随机blue [0,255]
     var blue = Math.ceil(Math.random() * 255)
     ctx.fillStyle = `rgb(${red},${green},${blue})`
-    //绘制字符到画布 x:[0,canWidth] y:[0,canHeight]
-    /*
-        每个字符占水平宽度最大值:canWidth/charCount
-        第一个字符:
-            x:[0,canWidth/charCount-fontSize]  y:[fontSize,canHeight]
-        第二个字符:
-            x:[canWidth/charCount,2*(canWidth/charCount)-fontSize] y:[fontSize,canHeight]
-        第三个字符:
-            x:[canWidth/charCount*2,3*(canWidth/charCount)-fontSize] y:[fontSize,canHeight]
-        第四个字符:
-            x:[canWidth/charCount*3,4*(canWidth/charCount)-fontSize] y:[fontSize,canHeight]
-        */
-    //获取每个字符的单元宽度
     var charCeilSize = canWidth / charCount
-    //[index*(charCeilSize),(index+1)(charCeilSize)-fontSize]
     var x =
       Math.ceil(
         Math.random() *
           ((index + 1) * charCeilSize - fontSize - index * charCeilSize)
       ) +
       index * charCeilSize
-    //[fontSize,canHeight]
     var y = Math.ceil(Math.random() * (canHeight - fontSize)) + fontSize
     ctx.fillText(char, x, y)
   }
 }
-//生成指定位数的随机字符
 function createRandomChars(charCount) {
-  //定义字符串，存储验证码字符
   var charList =
     'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
-  //存储随机字符
   var randomChar = []
   for (var i = 0; i < charCount; i++) {
-    //随机索引[0,len)
     var index = Math.floor(Math.random() * charList.length)
-    //获取索引对应字符
     var char = charList.charAt(index)
-    //存储随机字符
     randomChar.push(char)
   }
   return randomChar
---
+++ assets/main.js
@@ -0,0 +1,160 @@
+function copyTextToClipboard(text) {
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      alert('复制成功，请粘贴到迅雷去下载')
+    })
+    .catch((e) => {
+      alert('地址解析，失败请重试')
+    })
+}
+
+function render(list) {
+  const arr = []
+
+  list.forEach(({ box, disk }, i) => {
+    if (i > 3) return
+
+    arr.push(`<h3 class="box">${box}</h3>`)
+
+    for (let { type, link } of disk) {
+      link.forEach((item, i) => {
+        arr.push(
+          `<p>${type} <a onclick="verify('${item.href}','${box}</br>${item.name}')">${item.name}</a></p>`
+        )
+      })
+    }
+  })
+
+  if (!list.length) {
+    arr.push('Oh~ Sorry, No data.')
+  }
+
+  document.querySelector('.box').innerHTML = arr.join('')
+}
+
+fetch('assets/table.json')
+  .then((res) => res.json())
+  .then((res) => render(res))
+
+function cancel() {
+  document.body.classList.toggle('lock')
+  var dialog = document.querySelector('.dialog')
+  document.body.removeChild(dialog)
+}
+function lock() {
+  document.body.classList.toggle('lock')
+}
+
+function verify(url, name) {
+  const div = document.createElement('div')
+  div.className = 'dialog'
+  div.innerHTML = `
+  <div class="mask"></div>
+  <div class="win">
+    <h5 class="title">${name}</h5>
+    <div class="dialog-box">
+      <canvas></canvas>
+      <input type="text" maxlength="10">
+      <a class="check">确定</a>&nbsp;&nbsp;
+      <a onclick="cancel()">退出</a>
+    </div>
+  </div>
+  `
+  document.body.appendChild(div)
+  lock()
+
+  var randomChars = randomChar()
+  document.querySelector('canvas').onclick = function () {
+    randomChars = randomChar()
+  }
+
+  document.querySelector('.check').onclick = function () {
+    const val = document.querySelector('input').value.toLocaleLowerCase()
+
+    if (val == randomChars.toLocaleLowerCase()) {
+      loading(url)
+    } else {
+      randomChars = randomChar()
+    }
+  }
+}
+
+function loadData(url, cb) {
+  // try {
+  //   await fetch('https://jazzy-alfajores-2f455d.netlify.app')
+  // } catch (e) {
+  //   document.querySelector('.link').innerHTML = error
+  //   return
+  // }
+
+  const data = {
+    method: 'POST',
+    body: JSON.stringify({ disk: url })
+  }
+  var api = 'https://jazzy-alfajores-2f455d.netlify.app/.netlify/functions/p3'
+  return fetch(api, data)
+    .then((res) => res.json())
+    .then((res) => cb(res))
+}
+
+function mockData(url, cb) {
+  const data = {
+    error: [],
+    files: [
+      {
+        name: 'test_download_aosp_15_4_1.7z',
+        size: 783572280,
+        blobStorage:
+          'magnet:?xt=urn:btih:31594780a8889367ae2bf5fbec218f2503a35ae4',
+        modified: '2012/9/18 01:12:19'
+      },
+      {
+        name: 'sbx_dealdig_boxd6_aosp_15_4_1.7z',
+        size: 713272280,
+        blobStorage:
+          'https://www.npmjs.com/package/litee64c26fc945bb5681f4b7d91fcc2032afa549616504c05ad0b93656c60dec80/635ab1c1/Arlo4ikbYaCSRCfmCwxQEVczWv8M8YpFDoBdoAIqzoINLZSr7w60gJP2DpWFJNaKNDUryjaONY0GW4RhGeR_wg%3D%3D?uid=0&filename=ugsbx_ugoos_x2_atv_042_9.7z&disposition=attachment&hash=ysKPQC6obIABL8sNuBMSfzDWiMVAj9NJ3xmkCKo%2BSErDQe9%2B16VZULwZb0RFX1Cnq/J6bpmRyOJonT3VoXnDag%3D%3D%3A/ugsbx_ugoos_x2_atv_042_9.7z&limit=0&content_type=application%2Fx-7z-compressed&owner_uid=40520828&fsize=776415887&hid=0ab2996e34cf1bce9168914cf0420743&media_type=compressed&tknv=v2',
+        modified: '2012/9/18 01:12:19'
+      },
+      {
+        name: 'ugsbx_ugoos_am6_encrypted_atv_042_9.7z',
+        size: 783272280,
+        blobStorage:
+          'https://www.npmjs.com/pfdas593e19964bcfed7940ea432fce5e2fc76642ebae1e8676647b68ef13d80bf76d/635ab1be/Arlo4ikbYaCSRCfmCwxQETgj6GaxQBp89_o3evMLIeE71jTN4MOAUzAEpMF_SeFGsSah89lRtI2gD00wts4FBA%3D%3D?uid=0&filename=ugsbx_ugoos_x2_encrypted_aosp_053_1.7z.uploadtmp&disposition=attachment&hash=Luo48wER2Ps7JrNm7R0LQuNjyxLVFedPCFphuGECnMHzjnhd05fXWXgMIx8rouggq/J6bpmRyOJonT3VoXnDag%3D%3D%3A/ugsbx_ugoos_x2_encrypted_aosp_053_1.7z.uploadtmp&limit=0&content_type=application%2Fx-7z-compressed&owner_uid=40520828&fsize=837194756&hid=65223fa428da7de0cddabf64e6256ca6&media_type=compressed&tknv=v2',
+        modified: '2022/9/18 03:11:59'
+      }
+    ]
+  }
+
+  cb(data)
+}
+
+async function loading(url) {
+  const error = `解析失败 <a onclick="loading('${url}')">重试</a> <a onclick="cancel()">退出</a>`
+
+  var box = document.querySelector('.dialog-box')
+
+  box.innerHTML = '<p>正在解析...&nbsp;&nbsp;请等待<p>'
+
+  loadData(url, (res) => {
+    const list = []
+    if (res && res.files) {
+      res.files.forEach((obj, i) => {
+        const mb = (obj.size / 1024 / 1024).toFixed(2) + 'M'
+        if (obj.blobStorage) {
+          list.push(
+            `<div class="list"><p><a onclick="copyTextToClipboard('${obj.blobStorage}')">${obj.name}</a></p><info>发布于${obj.modified}</info></div>`
+          )
+        }
+      })
+    }
+
+    if (list.length) {
+      list.push('<br/><a onclick="cancel()">退出</a>')
+    }
+
+    box.innerHTML = list.length ? list.join('') : error
+  }).catch(() => {
+    box.innerHTML = error
+  })
+}
